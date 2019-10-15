ce169d384ec73170abe32bc7fedf25b812b12321
jiangyu <jiangyuwise@gmail.com>
2019-10-15 09:45:48 +0800

    改变文件结构

--- chapter02/hello-world/src/main/java/com/codve/prospring/ch02/xml/HelloWorldSpringDI.java
+++ chapter02/hello-world/src/main/java/com/codve/prospring/ch02/xml/HelloWorldSpringDI.java
@@ -1,16 +1,16 @@
-package com.codve.prospring.ch02.xmlconfig;
+package com.codve.prospring.ch02.xml;
 
 import com.codve.prospring.ch02.decoupled.MessageRender;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * 使用标准的 XML 注解 bean, 相应的配置文件为 /resources/xmlconfig/app.xml
+ * 使用标准的 XML 注解 bean, 相应的配置文件为 /resources/xml/app.xml
  */
 public class HelloWorldSpringDI {
     public static void main(String[] args) {
         ApplicationContext context = new
-                ClassPathXmlApplicationContext("xmlconfig/app.xml");
+                ClassPathXmlApplicationContext("xml/app.xml");
         MessageRender render = context.getBean("render", MessageRender.class);
         render.render();
     }
