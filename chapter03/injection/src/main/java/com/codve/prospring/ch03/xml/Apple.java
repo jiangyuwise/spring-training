5da8b58596886518ea49010d6d8211abb9403b77
jiangyu <jiangyuwise@gmail.com>
2019-10-16 10:38:35 +0800

    复习

--- chapter03/injection/src/main/java/com/codve/prospring/ch03/xml/Apple.java
+++ chapter03/injection/src/main/java/com/codve/prospring/ch03/xml/Apple.java
@@ -1,7 +1,14 @@
 package com.codve.prospring.ch03.xml;
 
+/**
+ * Apple 使用了构造函数注入, 见 xml/app1.xml
+ */
 public class Apple implements Fruit{
-    private String name = "apple";
+    private String name;
+
+    public Apple(String name) {
+        this.name = name;
+    }
 
     @Override
     public String getName() {
